16cdb1a4352717543f54682e3e16b8ce9e430744
Márcio Corrêa <marcio.ramos.correa@gmail.com>
2025-07-10 09:25:15 -0300

    Adicionando arquivo inicial com teste de login

---
+++ test/login.test.js
@@ -0,0 +1,35 @@
+import http from 'k6/http'
+import { check, sleep } from 'k6'
+
+export const options = {
+  iterations: 50,
+  thresholds: {
+    http_req_failed: ['rate<0.01'], 
+    http_req_duration: ['p(90)<20', 'max<9'],
+  }
+}
+
+export default function () {
+
+  const url = 'http://localhost:3000/login'
+
+  const payload = JSON.stringify({
+    username: 'julio.lima',
+    senha: '123456'
+  })
+
+  const params = {
+    headers: {
+        'Content-Type': 'application/json'
+    }
+  }
+
+  const response = http.post(url, payload, params)
+
+  check(response, {
+    'Validar que o status é 200': (r) => r.status === 200,
+    'Validar que o token é uma string': (r) => typeof(r.json().token) === 'string'
+  })
+
+  sleep(1)
+}
