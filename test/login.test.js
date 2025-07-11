e2e65dd37e5ee12d9cdfed0cfd3d79226764e271
Márcio Corrêa <marcio.ramos.correa@gmail.com>
2025-07-10 23:07:42 -0300

    usando stages

--- test/login.test.js
+++ test/login.test.js
@@ -3,8 +3,15 @@ import { check, sleep } from 'k6'
 
 export const options = {
   // iterations: 50,
-  vus: 10,
-  duration: '30s',
+  // vus: 10,
+  // duration: '30s',
+  stages: [
+    { duration: '10s', target: 10 },
+    { duration: '20s', target: 10 },
+    { duration: '10s', target: 30 },
+    { duration: '20s', target: 30 },
+    { duration: '20s', target: 0 },
+  ],
   thresholds: {
     http_req_failed: ['rate<0.01'], 
     http_req_duration: ['p(90)<20', 'max<9'],
